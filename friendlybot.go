e26a4c1df5e313d190973d866333420956901b70
Corbett Rider <corbmr@gmail.com>
2018-10-14 15:24:12 -0400

    Formatted and finished up

--- friendlybot.go
+++ friendlybot.go
@@ -1,201 +1,187 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "os"
-    "os/signal"
-    "flag"
-    "sync"
-    . "github.com/bwmarrin/discordgo"
+	"fmt"
+	. "github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
 )
 
 var Token string
 
 func init() {
-    flag.StringVar(&Token, "t", "", "Bot token")
-    flag.Parse()
+	Token = os.Getenv("$FRIENDTOKEN")
 
-    if Token == "" {
-        flag.Usage()
-        os.Exit(1)
-    }
+	if Token == "" {
+		fmt.Println("FRIENDTOKEN environment variable not set")
+		os.Exit(1)
+	}
 }
 
 func main() {
-    dg, err := New("Bot " + Token)
-    if err != nil {
-        fmt.Println("error creating Discord session,", err)
-        return
-    }
+	dg, err := New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
 
-    dg.AddHandler(ReadyHandler)
-    dg.AddHandler(GuildJoinHandler)
-    dg.AddHandler(MessageCreateHandler)
+	dg.AddHandler(readyHandler)
+	dg.AddHandler(guildJoinHandler)
+	dg.AddHandler(messageCreateHandler)
 
-    err = dg.Open()
-    if err != nil {
-    fmt.Println("error opening connection,", err)
-        return
-    }
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
 
-    defer dg.Close()
+	defer dg.Close()
 
-    fmt.Println("Friendlybot now running")
+	fmt.Println("Friendlybot now running")
 
-    s := make(chan os.Signal, 1)
-    signal.Notify(s, os.Interrupt, os.Kill)
-    <-s
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt, os.Kill)
+	<-s
 }
 
-type Game int
+type game struct {
+	roleName, command string
+	color             int
+}
 
-const (
-	Melee Game = iota
-	Smash4
-    GameCount
-)
-
-var RoleNames = [GameCount]string{
-    "Melee Friendlies",
-    "Smash 4 Friendlies",
+var games = [...]game{
+	game{"melee-friends", "melee", 0xE65F47},
+	game{"smash4-friends", "smash4", 0x479CF1},
 }
 
 type (
-    GuildID = string
-    RoleID = string
+	guildID = string
+	roleID  = string
 )
 
-var RoleMap sync.Map
+var roleMap = make(map[guildID][]roleID)
+var rw sync.RWMutex
 
-func ReadyHandler(s *Session, r *Ready) {
+func readyHandler(s *Session, r *Ready) {
 
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 	for _, g := range r.Guilds {
-        wg.Add(1)
-        go func(g *Guild) {
-            defer wg.Done()
-            GetOrAddRoles(s, g)
-        }(g)
+		wg.Add(1)
+		go func(g *Guild) {
+			defer wg.Done()
+			getOrAddRoles(s, g)
+		}(g)
 	}
-    wg.Wait()
+	wg.Wait()
 
 }
 
-func GetOrAddRoles(s *Session, g *Guild) {
+func getOrAddRoles(s *Session, g *Guild) {
 
-    var found [GameCount]RoleID
+	roles, err := s.GuildRoles(g.ID)
+	if err != nil {
+		fmt.Println("Unable to get guild roles", err)
+		return
+	}
 
-    roles, err := s.GuildRoles(g.ID)
-    if err != nil {
-        fmt.Println("Unable to get guild roles", err)
-        return
-    }
+	found := make([]roleID, len(games))
 
-    for _, role := range roles {
+	for _, r := range roles {
+		for i, g := range games {
+			if r.Name == g.roleName {
+				found[i] = r.ID
+				break
+			}
+		}
+	}
 
-        switch role.Name {
-        case RoleNames[Melee]:
-            found[Melee] = role.ID
-            fmt.Println("Found existing melee role")
+	for i := range found {
+		if found[i] == "" {
+			r, err := s.GuildRoleCreate(g.ID)
+			if err != nil {
+				fmt.Printf("Unable to create role %v for guild %s, %v\n", games[i].roleName, g.Name, err)
+			}
 
-        case RoleNames[Smash4]:
-            found[Smash4] = role.ID
-            fmt.Println("Found existing smash 4 role")
-        }
+			r, err = s.GuildRoleEdit(g.ID, r.ID, games[i].roleName, games[i].color, false, 0, true)
+			if err != nil {
+				fmt.Printf("Unable to edit role %v for guild %s, %v\n", games[i].roleName, g.Name, err)
+			}
 
-    }
+			found[i] = r.ID
+		}
+	}
 
-    if found[Melee] == "" {
-        r, err := s.GuildRoleCreate(g.ID)
-        if err != nil {
-            fmt.Println("Unable to create melee role for guild", g.Name, err)
-        }
-
-        r, err = s.GuildRoleEdit(g.ID, r.ID, RoleNames[Melee], 0xFF0000, false, 0, true)
-        if err != nil {
-            fmt.Println("Unable to change melee role")
-        }
-    }
-
-    if found[Smash4] == "" {
-        r, err := s.GuildRoleCreate(g.ID)
-        if err != nil {
-            fmt.Println("Unable to create smash4 role for guild", g.Name, err)
-        }
-
-        r, err = s.GuildRoleEdit(g.ID, r.ID, RoleNames[Smash4], 0x0000FF, false, 0, true)
-        if err != nil {
-            fmt.Println("Unable to change smash 4 role")
-        }
-    }
-
-    RoleMap.Store(g.ID, found)
+	rw.Lock()
+	roleMap[g.ID] = found
+	rw.Unlock()
 }
 
-func GuildJoinHandler(s *Session, g *GuildCreate) {
-    GetOrAddRoles(s, g.Guild)
+func guildJoinHandler(s *Session, g *GuildCreate) {
+	getOrAddRoles(s, g.Guild)
 }
 
-func MessageCreateHandler(s *Session, m *MessageCreate) {
+func messageCreateHandler(s *Session, m *MessageCreate) {
 
-    command := strings.Fields(m.Content)
+	command := strings.Fields(m.Content)
 
-    if len(command) < 2 || command[0] != "!f" {
-        return
-    }
+	if len(command) < 2 || command[0] != "!f" {
+		return
+	}
 
-    switch command[1] {
-    case "melee":
-        ToggleRole(s, m.ChannelID, m.Author, Melee)
-    case "smash4":
-        ToggleRole(s, m.ChannelID, m.Author, Smash4)
+	if command[1] == "list" {
+		s.ChannelMessageSend(m.ChannelID, "Supported games are melee and smash4")
+		return
+	}
 
-    default:
-        s.ChannelMessageSend(m.ChannelID, "Unknown game. Valid options are melee or smash4")
-    }
+	for i, g := range games {
+		if command[1] == g.command {
+			toggleRole(s, m.ChannelID, m.Author, i)
+			return
+		}
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "Unknown game.")
 
 }
 
-func ToggleRole(s *Session, chID string, u *User, game Game) {
-    ch, err := s.Channel(chID)
-    if err != nil {
-        fmt.Println("Unable to get channel", err)
-        return
-    }
+func toggleRole(s *Session, chID string, u *User, g int) {
+	ch, err := s.Channel(chID)
+	if err != nil {
+		fmt.Println("Unable to get channel", err)
+		return
+	}
 
-    m, err := s.GuildMember(ch.GuildID, u.ID)
-    if err != nil {
-        fmt.Println("Unable to get member", err)
-        return
-    }
+	m, err := s.GuildMember(ch.GuildID, u.ID)
+	if err != nil {
+		fmt.Println("Unable to get member", err)
+		return
+	}
 
-    rs, ok := RoleMap.Load(ch.GuildID)
-    if !ok {
-        fmt.Println("Unable to load role", err)
-    }
+    rw.RLock()
+	rID := roleMap[ch.GuildID][g]
+    rw.RUnlock()
 
-    rID := rs.([GameCount]RoleID)[game]
+	has := false
+	for _, r := range m.Roles {
+		if r == rID {
+			has = true
+			break
+		}
+	}
 
-    has := false
-    for _, r := range m.Roles {
-        if r == rID {
-            has = true
-            break
-        }
-    }
+	if has {
+		err = s.GuildMemberRoleRemove(ch.GuildID, u.ID, rID)
+	} else {
+		err = s.GuildMemberRoleAdd(ch.GuildID, u.ID, rID)
+	}
 
-    if has {
-        err = s.GuildMemberRoleRemove(ch.GuildID, u.ID, rID)
-    } else {
-        err = s.GuildMemberRoleAdd(ch.GuildID, u.ID, rID)
-    }
+	if err != nil {
+		fmt.Println("Unable to change role")
+		return
+	}
 
-    if err != nil {
-        fmt.Println("Unable to change role")
-        return
-    }
-
-    s.ChannelMessageSend(chID, "Role changed successfully")
+	s.ChannelMessageSend(chID, "Role changed successfully")
 
 }
